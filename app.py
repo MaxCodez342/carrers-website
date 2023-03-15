00e25e1b56a1ed6704dfe507f1cc5e4f29e1b073
MaxCodez342 <127662806+MaxCodez342@users.noreply.github.com>
2023-03-15 10:19:22 +0000

    added new html code

--- app.py
+++ app.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'ID':1,
+  'Title': 'Data analyst',
+  'Location' : 'Bengaluru India',
+  "Salary":"₹1,000,000"
+   },
+  {
+  'ID':2,
+  'Title': 'Data Scientist',
+  'Location' : 'Delhi India',
+  "Salary":"₹1,000,000"
+},
+{
+  'ID':3,
+  'Title': 'Front End Engeineer',
+  'Location' : 'Remote',
+  "Salary":"₹1,000,000"
+},
+{
+  'ID':4,
+  'Title': 'Back End Engeineer',
+  'Location' : 'San Francisco , USA',
+  "Salary":"$120,000"
+}
+
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",jobs=JOBS , Company_name="Max")
 
 
 if __name__ == "__main__":
---
+++ templates/if tag in html.md
@@ -0,0 +1,3 @@
+{% if 'salary' in job%}
+**part to be rendered**
+{%end if%}
